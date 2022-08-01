edc5e7d9cf5c73115ee59f25c2e604d0265da48c
Paula Morales <pmorales@slingr.io>
2022-08-01 16:26:32 -0300

    Access token on endpoint started

--- src/main/java/io/slingr/endpoints/webflow/WebflowEndpoint.java
+++ src/main/java/io/slingr/endpoints/webflow/WebflowEndpoint.java
@@ -7,11 +7,13 @@ import io.slingr.endpoints.exceptions.EndpointException;
 import io.slingr.endpoints.framework.annotations.*;
 import io.slingr.endpoints.services.AppLogs;
 import io.slingr.endpoints.services.HttpService;
+import io.slingr.endpoints.services.datastores.DataStore;
 import io.slingr.endpoints.utils.Json;
 import io.slingr.endpoints.ws.exchange.FunctionRequest;
 import io.slingr.endpoints.ws.exchange.WebServiceRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 /**
  * Webflow endpoint
@@ -23,6 +25,9 @@ public class WebflowEndpoint extends HttpEndpoint {
     private static final Logger logger = LoggerFactory.getLogger(WebflowEndpoint.class);
     private static final String WEBFLOW_API = "https://api.webflow.com";
     
+    @EndpointDataStore(name = TokenManager.DATA_STORE)
+    private DataStore tokensDataStore;
+
     @EndpointProperty
     private String clientId;
 
@@ -32,9 +37,8 @@ public class WebflowEndpoint extends HttpEndpoint {
     @EndpointProperty
     private String authorizationCode;
 
-    @EndpointProperty
     private String accessToken;
-
+   
     @ApplicationLogger
     private AppLogs appLogger;
 
@@ -43,11 +47,23 @@ public class WebflowEndpoint extends HttpEndpoint {
         return WEBFLOW_API;
     }
 
+
+    private TokenManager tokenManager;    
+
     @Override
     public void endpointStarted() {
         EndpointProperties properties =  properties();
         String redirectUri = "https://"+properties.getApplicationName()+"."+properties.getBaseDomain() + "/callback";
         appLogger.info("Redirect uri: " + redirectUri);
+        try {
+            appLogger.info("Setting token on endpoint start");
+            this.tokenManager = new TokenManager(httpService(), tokensDataStore, clientId, clientSecret, authorizationCode, redirectUri);
+            appLogger.info("Token succefully set up on endpoint start");
+        } catch (Exception e) {
+            appLogger.error(String.format("Error getting token for client ID [%s]. Refresh authorization code and redeploy the endpoint.", clientId),e);
+            appLogger.error(e.getMessage());
+            throw e;
+        }
     }
 
     @EndpointWebService(path = "/")
@@ -60,7 +76,6 @@ public class WebflowEndpoint extends HttpEndpoint {
     @EndpointFunction(name = "_get")
     public Json get(FunctionRequest request) {
         try {
-            setRequestHeaders(request);
             return defaultGetRequest(request);
         } catch (EndpointException restException) {
             throw restException;
@@ -70,7 +85,6 @@ public class WebflowEndpoint extends HttpEndpoint {
     @EndpointFunction(name = "_post")
     public Json post(FunctionRequest request) {
         try {
-            setRequestHeaders(request);
             return defaultPostRequest(request);
         } catch (EndpointException restException) {
             throw restException;
@@ -80,7 +94,6 @@ public class WebflowEndpoint extends HttpEndpoint {
     @EndpointFunction(name = "_put")
     public Json put(FunctionRequest request) {
         try {
-            setRequestHeaders(request);
             return defaultPutRequest(request);
         } catch (EndpointException restException) {
             throw restException;
@@ -90,7 +103,6 @@ public class WebflowEndpoint extends HttpEndpoint {
     @EndpointFunction(name = "_patch")
     public Json patch(FunctionRequest request) {
         try {
-            setRequestHeaders(request);
             return defaultPatchRequest(request);
         } catch (EndpointException restException) {
             throw restException;
@@ -100,20 +112,9 @@ public class WebflowEndpoint extends HttpEndpoint {
     @EndpointFunction(name = "_delete")
     public Json delete(FunctionRequest request) {
         try {
-            setRequestHeaders(request);
             return defaultDeleteRequest(request);
         } catch (EndpointException restException) {
             throw restException;
         }
-    }
-
-    private void setRequestHeaders(FunctionRequest request) {
-        Json body = request.getJsonParams();
-        Json headers = body.json("headers");
-        if (headers == null) {
-            headers = Json.map();
-        }
-        headers.set("Authorization", "Bearer " + accessToken);
-        body.set("headers", headers);
     }
 }
